7f1def70426e8b5704599d2e989e5db5461aa9ac
Francesco Ceccon <francesco@ceccon.me>
2022-12-08 15:24:37 +0000

    protocol: reconnect stream on disconnect

--- examples/simple-client/src/main.ts
+++ examples/simple-client/src/main.ts
@@ -1,18 +1,63 @@
-import { NodeClient, credentials } from '@apibara/protocol'
+import { NodeClient, credentials, proto } from '@apibara/protocol'
 import { StreamMessagesResponse__Output } from '@apibara/protocol/dist/proto/apibara/node/v1alpha1/StreamMessagesResponse'
 import { Block } from '@apibara/starknet'
 
+/**
+ * AppIndexer is used to keep track of the indexer's state between different
+ * calls.
+ */
+class AppIndexer {
+  // protobuf encodes possibly-large numbers as strings
+  private currentSequence?: string
+
+  handleData(data: proto.Data__Output) {
+    // track sequence number for reconnecting later
+    this.currentSequence = data.sequence
+    if (!data.data?.value) {
+      return
+    }
+    const block = Block.decode(data.data.value)
+    console.log(`[data] blockNumber=${block.blockNumber}`)
+  }
+
+  handleInvalidate(invalidate: proto.Invalidate__Output) {
+    console.log(`[invalidate] sequence=${invalidate.sequence}`)
+    this.currentSequence = invalidate.sequence
+  }
+
+  onRetry(retryCount: number) {
+    // retry connecting up to 3 times, with a delay of 5 seconds in between
+    // retries.
+    // Start from the sequence number _following_ the last received message.
+    const retry = retryCount < 3
+    const startingSequence = this.currentSequence ? +this.currentSequence + 1 : undefined
+
+    console.log(`[retry] retry=${retry ? 'yes' : 'no'}, startingSequence=${startingSequence}`)
+
+    return { retry, delay: 5, startingSequence }
+  }
+}
+
 async function main() {
+  const indexer = new AppIndexer()
   const node = new NodeClient('goerli.starknet.stream.apibara.com:443', credentials.createSsl())
-  const messages = node.streamMessages({})
+
+  const messages = node.streamMessages(
+    { startingSequence: 100_000 },
+    {
+      reconnect: true,
+      onRetry: indexer.onRetry,
+    }
+  )
+
   return new Promise((resolve, reject) => {
     messages.on('end', resolve)
     messages.on('error', reject)
-    messages.on('data', (data: StreamMessagesResponse__Output) => {
-      const value = data.data?.data?.value
-      if (value) {
-        const block = Block.decode(value)
-        console.log(`${block.blockNumber} ${block.transactions.length}`)
+    messages.on('data', (msg: StreamMessagesResponse__Output) => {
+      if (msg.data) {
+        indexer.handleData(msg.data)
+      } else if (msg.invalidate) {
+        indexer.handleInvalidate(msg.invalidate)
       }
     })
   })
--- packages/protocol/src/client.ts
+++ packages/protocol/src/client.ts
@@ -1,4 +1,5 @@
 import { promisify } from 'util'
+import { Readable } from 'stream'
 import {
   loadPackageDefinition,
   credentials as grpcCredentials,
@@ -18,9 +19,23 @@ const __NODE_PROTO_PATH = __dirname + '/proto/node.proto'
 const packageDefinition = loadSync(__NODE_PROTO_PATH, {})
 const protoDescriptor = loadPackageDefinition(packageDefinition) as unknown as ProtoGrpcType
 
+const defaultMaxRetry = 3
+const defaultDelaySeconds = 5
+
 export const Node = protoDescriptor.apibara.node.v1alpha1.Node
 
 export const credentials = grpcCredentials
+
+export interface Retry {
+  retry: boolean
+  startingSequence?: number
+  delay?: number
+}
+
+export interface StreamMessagesOptions {
+  reconnect?: boolean
+  onRetry?: (retryCount: number) => Retry
+}
 
 export class NodeClient {
   private readonly client: GrpcNodeClient
@@ -34,8 +49,132 @@ export class NodeClient {
   }
 
   public streamMessages(
+    args: StreamMessagesRequest,
+    options?: StreamMessagesOptions
+  ): StreamMessagesStream {
+    // only reconnect if user has opted-in
+    let onRetry = neverRetry
+    if (options?.reconnect) {
+      if (options.onRetry) {
+        onRetry = options.onRetry
+      } else {
+        onRetry = defaultOnRetry
+      }
+    }
+
+    return new StreamMessagesStream({ args, onRetry, client: this.client })
+  }
+}
+
+function neverRetry(_retryCount: number): Retry {
+  return {
+    retry: false,
+  }
+}
+
+function defaultOnRetry(retryCount: number): Retry {
+  return {
+    retry: retryCount < defaultMaxRetry,
+    delay: defaultDelaySeconds,
+  }
+}
+
+class StreamMessagesStream extends Readable {
+  private readonly client: GrpcNodeClient
+  private readonly initialRequest: StreamMessagesRequest
+  private readonly onRetry: (retryCount: number) => Retry
+  private source?: ClientReadableStream<StreamMessagesResponse__Output>
+  private currentSequence?: number
+  private retryCount: number
+
+  constructor({
+    args,
+    onRetry,
+    client,
+  }: {
     args: StreamMessagesRequest
-  ): ClientReadableStream<StreamMessagesResponse__Output> {
-    return this.client.streamMessages(args)
+    onRetry: (retryCount: number) => Retry
+    client: GrpcNodeClient
+  }) {
+    super({ objectMode: true })
+    this.initialRequest = args
+    this.client = client
+    this.onRetry = onRetry
+    this.retryCount = 0
+  }
+
+  _construct(callback: (error?: Error | null | undefined) => void): void {
+    this.setupSource()
+    callback()
+  }
+
+  _read(_size: number): void {
+    if (this.source) {
+      // resume source data if not flowing
+      if (!this.source.readableFlowing) {
+        this.source.resume()
+      }
+    } else {
+      // create source stream
+      this.setupSource(this.currentSequence)
+    }
+  }
+
+  setupSource(startingSequence?: number) {
+    const request =
+      startingSequence !== undefined
+        ? { ...this.initialRequest, startingSequence }
+        : this.initialRequest
+    this.source = this.client.streamMessages(request)
+
+    this.source.on('data', this.onData.bind(this))
+    this.source.once('error', this.onError.bind(this))
+    this.source.once('end', this.onEnd.bind(this))
+  }
+
+  cleanupSource() {
+    if (!this.source) {
+      return
+    }
+
+    this.source.off('data', this.onData.bind(this))
+    this.source.off('error', this.onData.bind(this))
+    this.source.off('end', this.onEnd.bind(this))
+  }
+
+  onData(chunk: StreamMessagesResponse__Output) {
+    // reset retry count
+    this.retryCount = 0
+    if (chunk.data) {
+      this.currentSequence = +chunk.data.sequence
+    } else if (chunk.invalidate) {
+      this.currentSequence = +chunk.invalidate
+    }
+
+    // apply backpressure on stream
+    if (!this.push(chunk)) {
+      this.source?.pause()
+    }
+  }
+
+  onError(_err: any) {
+    this.cleanupSource()
+    this.retryCount += 1
+
+    const { retry, delay, startingSequence } = this.onRetry(this.retryCount)
+    if (retry) {
+      const intervalDelayMs = (delay ?? defaultDelaySeconds) * 1000
+      setTimeout(() => {
+        this.setupSource(startingSequence)
+      }, intervalDelayMs)
+    }
+  }
+
+  onEnd() {
+    // block streams never end
+    if (this.retryCount > 0) {
+      return
+    }
+    this.cleanupSource()
   }
 }
