a611d405839fe8d2021aa173e41d773b15f140b5
Francesco Ceccon <francesco@ceccon.me>
2022-12-08 16:25:32 +0000

    ci: test packages

---
+++ packages/protocol/src/stream.test.ts
@@ -0,0 +1,9 @@
+describe('StreamMessagesStream', () => {
+  it('produces data', async () => {})
+
+  describe('on error', () => {
+    it('produces an error by default', () => {})
+    it('reconnects', () => {})
+    it('produces an error after trying to reconnect', () => {})
+  })
+})
