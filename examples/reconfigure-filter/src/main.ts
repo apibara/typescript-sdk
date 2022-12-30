695c14f89a4a2169bc128bfff64bfb81a7dbacaf
Francesco Ceccon <francesco@ceccon.me>
2022-12-30 16:51:00 +0000

    examples: show how to reconfigure stream

--- examples/reconfigure-filter/src/main.ts
+++ examples/reconfigure-filter/src/main.ts
@@ -2,33 +2,89 @@ import { StreamClient, ChannelCredentials, v1alpha2 } from '@apibara/protocol'
 import { Filter, FieldElement, v1alpha2 as starknet } from '@apibara/starknet'
 import { hash } from 'starknet'
 
+const FACTORY_ADDR = FieldElement.fromBigInt(
+  '0x00dad44c139a476c7a17fc8141e6db680e9abc9f56fe249a105094c44382c2fd'
+)
+
+const PAIR_CREATED_BN = BigInt(hash.getSelectorFromName('PairCreated'))
+const PAIR_CREATED_KEY = FieldElement.fromBigInt(PAIR_CREATED_BN)
+
+const TRANSFER_BN = BigInt(hash.getSelectorFromName('Transfer'))
+const TRANSFER_KEY = FieldElement.fromBigInt(TRANSFER_BN)
+
+const pairs: starknet.IFieldElement[] = []
+
+function baseFilter() {
+  return Filter.create()
+    .withHeader()
+    .addEvent((ev) => ev.withFromAddress(FACTORY_ADDR).withKeys([PAIR_CREATED_KEY]))
+}
+
+function handleBatch(client: StreamClient, cursor: v1alpha2.ICursor | null, batch: Uint8Array[]) {
+  let pair_changed = false
+
+  for (let item of batch) {
+    const block = starknet.Block.decode(item)
+    for (let { transaction, event } of block.events) {
+      if (!event || !event.keys || !event.data || !transaction?.meta?.hash || !event.fromAddress) {
+        continue
+      }
+
+      const txHash = FieldElement.toHex(transaction.meta.hash)
+
+      const key = FieldElement.toBigInt(event.keys[0])
+      if (key == PAIR_CREATED_BN) {
+        pair_changed = true
+        const token0 = FieldElement.toHex(event.data[0])
+        const token1 = FieldElement.toHex(event.data[1])
+        const pair = FieldElement.toHex(event.data[2])
+        pairs.push(event.data[2])
+
+        console.log(`Pair Created:`)
+        console.log(`     Tx Hash: ${txHash}`)
+        console.log(`     Address: ${pair}`)
+        console.log(`      Token0: ${token0}`)
+        console.log(`      Token1: ${token1}`)
+      } else if (key == TRANSFER_BN) {
+        const pair = FieldElement.toHex(event.fromAddress)
+        const fromAddr = FieldElement.toHex(event.data[0])
+        const toAddr = FieldElement.toHex(event.data[1])
+
+        console.log(`Transfer Tkn:`)
+        console.log(`     Tx Hash: ${txHash}`)
+        console.log(`     Address: ${pair}`)
+        console.log(`        From: ${fromAddr}`)
+        console.log(`          To: ${toAddr}`)
+      }
+    }
+  }
+
+  if (pair_changed) {
+    const filter = baseFilter()
+    pairs.forEach((pair) => {
+      filter.addEvent((ev) => ev.withFromAddress(pair).withKeys([TRANSFER_KEY]))
+    })
+
+    client.configure({ filter: filter.encode(), cursor })
+  }
+}
+
 async function main() {
   console.log('Streaming JediSwap pools')
 
-  // Encode factory address to the wire format.
-  const factoryAddress = FieldElement.fromBigInt(
-    '0x0329d90b8dd38a089e2c4d1e1b6f0caab166f769bda267b7d4118f06b07fed87'
-  )
-
-  const pairCreatedKey = [FieldElement.fromBigInt(hash.getSelectorFromName('PairCreated'))]
-
-  const filter = Filter.create()
-    .addEvent((ev) => ev.withFromAddress(factoryAddress).withKeys(pairCreatedKey))
-    .encode()
+  const filter = baseFilter().encode()
 
   const client = new StreamClient({
     url: 'localhost:7171',
     credentials: ChannelCredentials.createInsecure(),
   }).connect()
 
-  client.configure({ filter, batchSize: 10, finality: v1alpha2.DataFinality.DATA_STATUS_FINALIZED })
+  // force use of batches with size 1 so that reconfiguring doesn't skip any block
+  client.configure({ filter, batchSize: 1, finality: v1alpha2.DataFinality.DATA_STATUS_FINALIZED })
 
   for await (const message of client) {
-    if (message.data?.data) {
-      for (let item of message.data.data) {
-        const block = starknet.Block.decode(item)
-        console.log(block)
-      }
+    if (message.data && message.data?.data) {
+      handleBatch(client, message.data.endCursor, message.data.data)
     }
   }
 }
