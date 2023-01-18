22704b40de8914e272626cf8a957e87d8e7e5965
Francesco Ceccon <francesco@ceccon.me>
2023-01-18 11:55:14 +0000

    example: fix eth address

--- examples/simple-client/src/main.ts
+++ examples/simple-client/src/main.ts
@@ -30,8 +30,7 @@ async function main() {
 
   // Encode address to the wire format.
   const address = FieldElement.fromBigInt(
-    // '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'
-    '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc8'
+    '0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7'
   )
 
   const transferKey = [FieldElement.fromBigInt(hash.getSelectorFromName('Transfer'))]
