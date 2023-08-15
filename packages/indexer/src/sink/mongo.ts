603cfa72bac2c3bc0de54a3fc046555f7165ae56
Francesco Ceccon <francesco@ceccon.me>
2023-08-15 21:15:36 +0200

    indexer: Add indexer type definitions

---
+++ packages/indexer/src/sink/mongo.ts
@@ -0,0 +1,12 @@
+/** MongoDB sink options */
+export type Mongo = {
+  sinkType: 'mongo';
+  sinkOptions: {
+    /** MongoDB connection string. */
+    connectionString?: string;
+    /** Target database name. */
+    database?: string;
+    /** Target collection name. */
+    collectionName?: string;
+  }
+}
---
+++ packages/indexer/src/starknet/index.ts
@@ -0,0 +1,10 @@
+export * from './filter'
+export * from './block'
+
+import { Filter } from './filter'
+
+/** Starknet network type and data filter. */
+export type Starknet = {
+  network: 'starknet';
+  filter: Filter;
+}
