3d665d54a1e34c1c35b2d5cbb7036e8ffdee24c3
Tony Stark <oktonystark8@gmail.com>
2023-12-08 07:45:11 -0600

    feat: added test & removed eventParser method

--- packages/indexer/src/starknet/parser.test.ts
+++ packages/indexer/src/starknet/parser.test.ts
@@ -1,48 +1,34 @@
 import { Contract } from './parser';
-import { beforeEach, describe, expect, it } from "vitest"
+import { describe, expect, it } from "vitest"
+import { FieldElement } from "./felt";
+import { EventAbi } from "starknet";
 
 describe('Contract class tests', () => {
-  const mockAbi = [
-    { name: 'event1', otherProps: '...' },
-    { name: 'event2', otherProps: '...' },
-    // Add more mock events as necessary
-  ];
+  const mockContractAddress: FieldElement = '0x0';
+  const mockEventAbi: EventAbi = {
+    type: 'event',
+    name: 'MockEvent',
+    kind: 'struct',
+    members: [{ name: 'param1', type: 'felt', kind: 'key' }],
+  };
 
-  const mockAddress = '0x123'; // Replace with a valid mock address if necessary
+  const mockContractAbi = [mockEventAbi];
 
-  let contract: Contract;
+  it('should create an EventFilter for a valid event name', () => {
+    const contract = new Contract(mockContractAddress, mockContractAbi);
+    const filter = contract.eventFilter('MockEvent');
 
-  beforeEach(() => {
-    // Instantiate the Contract before each test with mocked ABI and address
-    contract = new Contract(mockAddress, mockAbi);
+    expect(filter).toBeDefined();
+    expect(filter.fromAddress).toBe(mockContractAddress);
+    // Further assertions can be made based on the expected structure of EventFilter
   });
 
-  describe('eventFilter method', () => {
-    it('should return the correct event from the ABI', () => {
-      const eventName = 'event1';
-      const event = contract.eventFilter(eventName);
-      expect(event).toEqual(mockAbi.find(item => item.name === eventName));
-    });
+  it('should throw an error for an invalid event name', () => {
+    const contract = new Contract(mockContractAddress, mockContractAbi);
 
-    it('should throw an error if the event is not found in the ABI', () => {
-      const invalidEventName = 'nonExistentEvent';
-      expect(() => contract.eventFilter(invalidEventName)).toThrow(`Event ${invalidEventName} not found in contract ABI`);
-    });
+    expect(() => {
+      contract.eventFilter('InvalidEvent');
+    }).toThrow('Event InvalidEvent not found in contract ABI');
   });
 
-  // describe('eventParser method', () => {
-  //   it('should return a function that correctly parses an event and its transaction', () => {
-  //     const eventName = 'event1';
-  //     const mockEvent = { name: 'event1', data: 'some data' }; // Mock event object
-  //     const mockTransaction = { id: 'tx123', data: 'transaction data' }; // Mock transaction object
-
-  //     const parseFunction = contract.eventParser(eventName);
-  //     const result = parseFunction({ event: mockEvent, transaction: mockTransaction });
-
-  //     expect(result).toEqual({
-  //       event: mockAbi.find(item => item.name === eventName),
-  //       transaction: mockTransaction
-  //     });
-  //   });
-  // });
 });
--- packages/indexer/src/starknet/parser.ts
+++ packages/indexer/src/starknet/parser.ts
@@ -1,16 +1,11 @@
 import { FieldElement } from "./felt";
 import { EventFilter } from "./filter";
-import { EventWithTransaction } from "./block";
 
-import { type Abi, hash } from "starknet";
+import { type Abi, EventAbi, hash } from "starknet";
 
-interface EventFilterParser {
-  eventFilter(name: string): EventFilter;
-  eventParser(name: string): ({ event, transaction }: EventWithTransaction ) => any;
-}
 
 // The Contract class implements the EventFilterParser interface.
-export class Contract implements EventFilterParser {
+export class Contract {
   // Read-only properties for the contract's address and its ABI.
   readonly contractAddress: FieldElement;
   readonly contractAbi: Abi;
@@ -21,36 +16,30 @@ export class Contract implements EventFilterParser {
     this.contractAbi = contractAbi;
   }
 
+  private _findEvent(name: string): EventAbi | undefined {
+    // Find the event in the ABI matching the provided name.
+    const event: EventAbi | undefined = this.contractAbi.find(
+      (item) => item.type === "event" && item.name === name
+    );
+
+    return event;
+  }
+
   // Method to filter events based on their name from the contract's ABI.
   eventFilter(name: string): EventFilter {
-    // Find the event in the ABI matching the provided name.
-    const event: EventFilter = this.contractAbi.find((item) => item.name === name);
-    
+    const event = this._findEvent(name);
+
     // Throw an error if the event is not found in the ABI
     if (!event) {
       throw new Error(`Event ${name} not found in contract ABI`);
     }
 
     // Return the found event.
-    return event;
-  }
-
-  // Method to parse events, given their name, and return a function that processes 
-  // an event and its associated transaction.
-  eventParser(name: string): ({ event, transaction }: EventWithTransaction ) => any {
-    const event = this.eventFilter(name);
-
-    // Need clarity on how to get a transaction from an event.
-
-    return ({ transaction }: EventWithTransaction): any => {
-      // Construct a new object combining the event details with the transaction.
-      const newEvent: any = {
-        event: event,
-        transaction: transaction,
-      };
-
-      // Return the combined event and transaction object.
-      return newEvent;
+    return {
+      fromAddress: this.contractAddress,
+      keys: [hash.getSelectorFromName(event.name) as `0x${string}`],
+      includeTransaction: false,
+      includeReceipt: false,
     };
   }
 }
