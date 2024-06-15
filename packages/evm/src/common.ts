841e9361fb2842a85d66e164d105153c389b2ab2
jaipaljadeja <jadejajaipal5@gmail.com>
2024-06-15 18:33:48 +0530

    feat: added evm data definitions

--- packages/evm/src/common.ts
+++ packages/evm/src/common.ts
@@ -63,3 +63,59 @@ export const B256 = Schema.transform(B256Proto, _B256, {
 
 export const b256ToProto = Schema.encodeSync(B256);
 export const b256FromProto = Schema.decodeSync(B256);
+
+/** Wire representation of `U256`. */
+const U256Proto = Schema.Struct({
+  loLo: Schema.BigIntFromSelf,
+  loHi: Schema.BigIntFromSelf,
+  hiLo: Schema.BigIntFromSelf,
+  hiHi: Schema.BigIntFromSelf,
+});
+
+/** Data with length 256 bits. */
+export const U256 = Schema.transform(U256Proto, Schema.BigIntFromSelf, {
+  decode(value) {
+    const loLo = value.loLo.toString(16).padStart(16, "0");
+    const loHi = value.loHi.toString(16).padStart(16, "0");
+    const hiLo = value.hiLo.toString(16).padStart(16, "0");
+    const hiHi = value.hiHi.toString(16).padStart(16, "0");
+    return BigInt(`0x${loLo}${loHi}${hiLo}${hiHi}`);
+  },
+  encode(value) {
+    const hex = value.toString(16).padStart(64, "0");
+    const s = hex.length;
+    const hiHi = BigInt(`0x${hex.slice(s - 16, s)}`);
+    const hiLo = BigInt(`0x${hex.slice(s - 32, s - 16)}`);
+    const loHi = BigInt(`0x${hex.slice(s - 48, s - 32)}`);
+    const loLo = BigInt(`0x${hex.slice(s - 64, s - 48)}`);
+    return { loLo, loHi, hiLo, hiHi };
+  },
+});
+
+export const u256ToProto = Schema.encodeSync(U256);
+export const u256FromProto = Schema.decodeSync(U256);
+
+/** Wire representation of `U128`. */
+const U128Proto = Schema.Struct({
+  lo: Schema.BigIntFromSelf,
+  hi: Schema.BigIntFromSelf,
+});
+
+/** Data with length 128 bits. */
+export const U128 = Schema.transform(U128Proto, Schema.BigIntFromSelf, {
+  decode(value) {
+    const lo = value.lo.toString(16).padStart(32, "0");
+    const hi = value.hi.toString(16).padStart(32, "0");
+    return BigInt(`0x${lo}${hi}`);
+  },
+  encode(value) {
+    const hex = value.toString(16).padStart(64, "0");
+    const s = hex.length;
+    const hi = BigInt(`0x${hex.slice(s - 32, s)}`);
+    const lo = BigInt(`0x${hex.slice(s - 64, s - 32)}`);
+    return { lo, hi };
+  },
+});
+
+export const u128ToProto = Schema.encodeSync(U128);
+export const u128FromProto = Schema.decodeSync(U128);
