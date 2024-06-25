345cfc19d1ed60bfc05d310aed3b5cf622ea4791
jaipaljadeja <jadejajaipal5@gmail.com>
2024-06-25 20:39:55 +0530

    indexer: add KVStore tests

---
+++ packages/indexer/src/plugins/kv.test.ts
@@ -0,0 +1,72 @@
+import { type Database, open } from "sqlite";
+import sqlite3 from "sqlite3";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { KVStore } from "./kv";
+
+describe("KVStore", () => {
+  let db: Database<sqlite3.Database, sqlite3.Statement>;
+  let store: KVStore<{ data: bigint }>;
+  const key = "test_key";
+
+  beforeAll(async () => {
+    db = await open({ driver: sqlite3.Database, filename: ":memory:" });
+    await db.exec(`
+      CREATE TABLE IF NOT EXISTS kvs (
+        from_block INTEGER NOT NULL,
+        to_block INTEGER,
+        k TEXT NOT NULL,
+        v BLOB NOT NULL,
+        PRIMARY KEY (from_block, k)
+      );
+    `);
+    store = new KVStore(db, "finalized", { orderKey: 5_000_000n });
+  });
+
+  afterAll(async () => {
+    await db.close();
+  });
+
+  it("should put and get a value", async () => {
+    const value = { data: 0n };
+
+    await store.put(key, value);
+    const result = await store.get(key);
+
+    expect(result).toEqual(value);
+  });
+
+  it("should return undefined for non-existing key", async () => {
+    const result = await store.get("non_existent_key");
+    expect(result).toBeUndefined();
+  });
+
+  it("should update an existing value", async () => {
+    store = new KVStore(db, "finalized", { orderKey: 5_000_020n });
+
+    const value = { data: 50n };
+
+    await store.put(key, value);
+    const result = await store.get(key);
+
+    expect(result).toEqual(value);
+  });
+
+  it("should delete a value", async () => {
+    await store.del(key);
+    const result = await store.get(key);
+
+    expect(result).toBeUndefined();
+
+    const rows = await db.all(
+      `
+      SELECT from_block, to_block, k, v
+      FROM kvs
+      WHERE k = ?
+    `,
+      [key],
+    );
+
+    // Check that the old is correctly marked with to_block
+    expect(rows[0].to_block).toBe(Number(5_000_020n));
+  });
+});
