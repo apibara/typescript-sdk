d48edde6320b551c9d41939f58f47b090e660a95
jaipaljadeja <jadejajaipal5@gmail.com>
2024-08-08 20:13:35 +0530

    cli: add starknet indexer in cli playground

---
+++ packages/cli/playground/indexers/starknet.indexer.ts
@@ -0,0 +1,42 @@
+import { defineIndexer } from "@apibara/indexer";
+import { StarknetStream } from "@apibara/starknet";
+import type { ApibaraRuntimeConfig } from "apibara/types";
+import consola from "consola";
+import { hash } from "starknet";
+
+export default function indexer(runtimeConfig: ApibaraRuntimeConfig) {
+  consola.log("--> Starknet Indexer Runtime Config: ", runtimeConfig);
+  return defineIndexer(StarknetStream)({
+    streamUrl: "http://mainnet-v2.starknet.a5a.ch:7007",
+    finality: "accepted",
+    startingCursor: {
+      orderKey: 80_000n,
+    },
+    filter: {
+      events: [
+        {
+          fromAddress:
+            "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+          keys: [hash.getSelectorFromName("Transfer") as `0x${string}`],
+          includeReceipt: true,
+        },
+      ],
+    },
+    async transform({ block: { header, events } }) {
+      return [
+        {
+          blockNumber: header.blockNumber,
+          events,
+        },
+      ];
+    },
+    hooks: {
+      "sink:write"({ data }) {
+        consola.log("Wrote:", data[0].events.length, "Tranfer events");
+      },
+      "sink:flush"({ endCursor }) {
+        consola.log("Flushing", endCursor.orderKey);
+      },
+    },
+  });
+}
