1ea4f2d7f0c0d9e16bcac7aa27abbdd9a6e7e92c
jaipaljadeja <jadejajaipal5@gmail.com>
2024-09-07 23:09:29 +0530

    indexer: add pgTable wrapper

---
+++ packages/indexer/src/sinks/drizzle/utils.ts
@@ -0,0 +1,40 @@
+import type { Cursor } from "@apibara/protocol";
+import type { PgTableFn } from "drizzle-orm/pg-core";
+import { pgTable as drizzlePgTable } from "drizzle-orm/pg-core";
+import range from "postgres-range";
+import { Int8Range, int8range } from "./Int8Range";
+
+export const pgTable: PgTableFn = (name, columns, extraConfig?) => {
+  return drizzlePgTable(
+    name,
+    {
+      ...columns,
+      _cursor: int8range("_cursor").notNull(),
+    },
+    extraConfig,
+  );
+};
+
+export const getDrizzleCursor = (
+  cursor_range:
+    | [Cursor | undefined | null, Cursor | undefined | null]
+    | Cursor
+    | undefined,
+) => {
+  const isArray = Array.isArray(cursor_range);
+  const [lower, upper] = isArray ? cursor_range : [cursor_range, undefined];
+  let isNoUpperBound = false;
+  if (!lower) {
+    throw new Error("Lower bound cursor is required");
+  }
+  if (!upper) {
+    isNoUpperBound = true;
+  }
+  return new Int8Range(
+    new range.Range(
+      Number(lower.orderKey),
+      Number(upper?.orderKey),
+      range.RANGE_LB_INC | (isNoUpperBound ? range.RANGE_UB_INF : 0),
+    ),
+  );
+};
