56fbe7104a5f0e43cbba60f5ac2c6c1538140f4c
jaipaljadeja <jadejajaipal5@gmail.com>
2024-10-18 00:16:25 +0530

    example: add cli demo

---
+++ examples/cli/indexers/starknet.indexer.ts
@@ -0,0 +1,33 @@
+import { defineIndexer } from "@apibara/indexer";
+import { StarknetStream } from "@apibara/starknet";
+import type { ApibaraRuntimeConfig } from "apibara/types";
+import { hash } from "starknet";
+
+export default function (runtimeConfig: ApibaraRuntimeConfig) {
+  console.log("--> Starknet Indexer Runtime Config: ", runtimeConfig);
+  return defineIndexer(StarknetStream)({
+    streamUrl: "https://starknet.preview.apibara.org",
+    finality: "accepted",
+    startingCursor: {
+      orderKey: 80_000n,
+    },
+    filter: {
+      events: [
+        {
+          address:
+            "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7" as `0x${string}`,
+          keys: [hash.getSelectorFromName("Transfer") as `0x${string}`],
+          includeReceipt: true,
+        },
+      ],
+    },
+    async transform({ block: { header } }) {
+      console.log("Transforming block ", header?.blockNumber);
+    },
+    hooks: {
+      "handler:after": ({ endCursor }) => {
+        console.log("Handler After ", endCursor?.orderKey);
+      },
+    },
+  });
+}
