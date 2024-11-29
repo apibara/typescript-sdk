1f876d0be609b75e9a270fa98f68e9100c68caaa
Francesco Ceccon <francesco@ceccon.me>
2024-11-29 15:22:27 +0100

    indexer: add logger plugin

---
+++ packages/indexer/src/plugins/logger.ts
@@ -0,0 +1,30 @@
+import { type ConsolaInstance, type ConsolaReporter, consola } from "consola";
+import { useIndexerContext } from "../context";
+import { defineIndexerPlugin } from "./config";
+
+export type { ConsolaReporter, ConsolaInstance } from "consola";
+
+export function logger<TFilter, TBlock, TTxnParams>({
+  logger,
+}: { logger?: ConsolaReporter } = {}) {
+  return defineIndexerPlugin<TFilter, TBlock, TTxnParams>((indexer) => {
+    indexer.hooks.hook("run:before", () => {
+      const ctx = useIndexerContext();
+
+      if (logger) {
+        ctx.logger = consola.create({ reporters: [logger] });
+      } else {
+        ctx.logger = consola.create({});
+      }
+    });
+  });
+}
+
+export function useLogger(): ConsolaInstance {
+  const ctx = useIndexerContext();
+
+  if (!ctx?.logger)
+    throw new Error("Logger plugin is not available in context");
+
+  return ctx.logger;
+}
