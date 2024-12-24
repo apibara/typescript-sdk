f197dee1942ca2ffc50095932792fa2cd587042f
jaipaljadeja <jadejajaipal5@gmail.com>
2024-12-24 16:42:59 +0530

    plugin-mongo: add invalidation tests

--- packages/plugin-mongo/tests/storage.test.ts
+++ packages/plugin-mongo/tests/storage.test.ts
@@ -12,8 +12,21 @@ import { describe, expect, it } from "vitest";
 
 import { MongoClient } from "mongodb";
 import { mongoStorage, useMongoStorage } from "../src";
+import { getState } from "../src/persistence";
+import { withTransaction } from "../src/utils";
 
-describe("SQLite persistence", () => {
+type TestSchema = {
+  blockNumber: number;
+  key?: string;
+  count?: number;
+  data?: string;
+  _cursor?: {
+    from: number;
+    to: number | null;
+  };
+};
+
+describe("MongoDB persistence", () => {
   it("should store data with a cursor", async () => {
     const { db, client, dbName } = getRandomDatabase();
 
@@ -22,9 +35,11 @@ describe("SQLite persistence", () => {
         plugins: [mongoStorage({ client, dbName, collections: ["test"] })],
         async transform({ endCursor, block: { data } }) {
           const db = useMongoStorage();
-          const rows = await db.collection("test").find({}).toArray();
+          const collection = db.collection<TestSchema>("test");
 
-          await db.collection("test").insertOne({
+          const rows = await collection.find({}).toArray();
+
+          await collection.insertOne({
             blockNumber: Number(endCursor?.orderKey),
             count: rows.length,
             data,
@@ -83,8 +98,10 @@ describe("SQLite persistence", () => {
         plugins: [mongoStorage({ client, dbName, collections: ["test"] })],
         async transform({ endCursor, block: { data } }) {
           const db = useMongoStorage();
-          const rows = await db.collection("test").find({}).toArray();
-          const result = await db.collection("test").updateOne(
+          const collection = db.collection<TestSchema>("test");
+          const rows = await collection.find({}).toArray();
+
+          const result = await collection.updateOne(
             {
               key: "abc",
             },
@@ -97,10 +114,11 @@ describe("SQLite persistence", () => {
           );
 
           if (result.modifiedCount === 0) {
-            await db.collection("test").insertOne({
+            await collection.insertOne({
               key: "abc",
               blockNumber: Number(endCursor?.orderKey),
               count: rows.length,
+              data,
             });
           }
         },
@@ -115,18 +133,14 @@ describe("SQLite persistence", () => {
 
     await run(mockClient, indexer);
 
-    const result = await db.collection("test").find().toArray();
+    const result = await db
+      .collection("test")
+      .find()
+      .sort({ blockNumber: 1 })
+      .toArray();
+
     expect(result.map(({ _id, ...rest }) => rest)).toMatchInlineSnapshot(`
       [
-        {
-          "_cursor": {
-            "from": 5000002,
-            "to": null,
-          },
-          "blockNumber": 5000002,
-          "count": 1,
-          "key": "abc",
-        },
         {
           "_cursor": {
             "from": 5000000,
@@ -134,6 +148,7 @@ describe("SQLite persistence", () => {
           },
           "blockNumber": 5000000,
           "count": 0,
+          "data": "5000000",
           "key": "abc",
         },
         {
@@ -143,6 +158,17 @@ describe("SQLite persistence", () => {
           },
           "blockNumber": 5000001,
           "count": 1,
+          "data": "5000000",
+          "key": "abc",
+        },
+        {
+          "_cursor": {
+            "from": 5000002,
+            "to": null,
+          },
+          "blockNumber": 5000002,
+          "count": 1,
+          "data": "5000000",
           "key": "abc",
         },
       ]
@@ -157,17 +183,18 @@ describe("SQLite persistence", () => {
         plugins: [mongoStorage({ client, dbName, collections: ["test"] })],
         async transform({ endCursor }) {
           const db = useMongoStorage();
+          const collection = db.collection<TestSchema>("test");
 
-          const value = await db.collection("test").findOne({
+          const value = await collection.findOne({
             key: "abc",
           });
 
           if (value) {
-            await db.collection("test").deleteOne({
+            await collection.deleteOne({
               key: "abc",
             });
           } else {
-            await db.collection("test").insertOne({
+            await collection.insertOne({
               key: "abc",
               blockNumber: Number(endCursor?.orderKey),
             });
@@ -205,6 +232,201 @@ describe("SQLite persistence", () => {
         },
       ]
     `);
+  });
+
+  it("should select data", async () => {
+    const { db, client, dbName } = getRandomDatabase();
+
+    const indexer = getMockIndexer({
+      override: {
+        plugins: [mongoStorage({ client, dbName, collections: ["test"] })],
+        async transform({ endCursor }) {
+          const db = useMongoStorage();
+
+          const collection = db.collection<TestSchema>("test");
+
+          // Insert data with each message
+          await collection.insertOne({
+            key: "abc",
+            blockNumber: Number(endCursor?.orderKey),
+            data: `data${endCursor?.orderKey}`,
+          });
+
+          // Delete data at block 5000001
+          if (endCursor?.orderKey === 5000001n) {
+            await collection.deleteOne({
+              blockNumber: 5000000,
+            });
+          }
+
+          // we check at last block if the data is deleted
+          if (endCursor?.orderKey === 5000002n) {
+            const latestData = await collection.find({}).toArray();
+            expect(latestData.length).toBe(2);
+            expect(latestData.some((d) => d.blockNumber === 5000000)).toBe(
+              false,
+            );
+          }
+        },
+      },
+    });
+
+    const mockClient = new MockClient<MockFilter, MockBlock>(
+      (request, options) => {
+        return generateMockMessages(3);
+      },
+    );
+
+    await run(mockClient, indexer);
+  });
+
+  it("should invalidate data", async () => {
+    const { db, client, dbName } = getRandomDatabase();
+
+    const indexer = getMockIndexer({
+      override: {
+        plugins: [mongoStorage({ client, dbName, collections: ["test"] })],
+        async transform({ endCursor, block: { data } }) {
+          const db = useMongoStorage();
+          const collection = db.collection<TestSchema>("test");
+          const rows = await collection.find({}).toArray();
+
+          const result = await collection.updateOne(
+            {
+              key: "abc",
+            },
+            {
+              $set: {
+                blockNumber: Number(endCursor?.orderKey),
+                count: rows.length,
+              },
+            },
+          );
+
+          if (result.modifiedCount === 0) {
+            await collection.insertOne({
+              key: "abc",
+              blockNumber: Number(endCursor?.orderKey),
+              count: rows.length,
+              data,
+            });
+          }
+        },
+      },
+    });
+
+    const mockClient = new MockClient<MockFilter, MockBlock>(
+      (request, options) => {
+        return generateMockMessages(3, {
+          // we invalidate at 3rd (last and index 2) message
+          // and we invalidate cursor 5000001 (2nd message and index 1)
+          invalidate: { invalidateTriggerIndex: 2, invalidateFromIndex: 1 },
+        });
+      },
+    );
+
+    await run(mockClient, indexer);
+
+    const result = await db
+      .collection("test")
+      .find()
+      .sort({ blockNumber: 1 })
+      .toArray();
+
+    expect(result.map(({ _id, ...rest }) => rest)).toMatchInlineSnapshot(`
+      [
+        {
+          "_cursor": {
+            "from": 5000000,
+            "to": 5000001,
+          },
+          "blockNumber": 5000000,
+          "count": 0,
+          "data": "5000000",
+          "key": "abc",
+        },
+        {
+          "_cursor": {
+            "from": 5000001,
+            "to": null,
+          },
+          "blockNumber": 5000001,
+          "count": 1,
+          "data": "5000000",
+          "key": "abc",
+        },
+      ]
+    `);
+  });
+
+  it("should persist state", async () => {
+    const { db, client, dbName } = getRandomDatabase();
+    const indexerName = "persist-test";
+
+    const indexer = getMockIndexer({
+      override: {
+        plugins: [
+          mongoStorage({
+            client,
+            dbName,
+            collections: ["test"],
+            persistState: true,
+            indexerName,
+          }),
+        ],
+        async transform({ endCursor }) {
+          const db = useMongoStorage();
+
+          const collection = db.collection<TestSchema>("test");
+
+          // Insert data with each message
+          await collection.insertOne({
+            blockNumber: Number(endCursor?.orderKey),
+            data: `data${endCursor?.orderKey}`,
+          });
+
+          if (endCursor?.orderKey === 5000006n) {
+            throw new Error("Intentional error to test persistence");
+          }
+        },
+      },
+    });
+
+    const mockClient = new MockClient<MockFilter, MockBlock>(
+      (request, options) => {
+        return generateMockMessages(10);
+      },
+    );
+
+    await expect(run(mockClient, indexer)).rejects.toThrow(
+      "Intentional error to test persistence",
+    );
+
+    // Verify thrown transform data is not committed in db
+    const result = await db
+      .collection<TestSchema>("test")
+      .find()
+      .sort({ blockNumber: 1 })
+      .toArray();
+    expect(result).toHaveLength(6); // Should have 6 documents (0-5)
+    expect(result[result.length - 1].blockNumber).toBe(5000005);
+
+    await withTransaction(client, async (session) => {
+      const persistence = await getState({
+        db,
+        session,
+        indexerName,
+      });
+      expect(persistence).toMatchInlineSnapshot(`
+        {
+          "cursor": {
+            "orderKey": 5000005n,
+            "uniqueKey": null,
+          },
+          "filter": undefined,
+        }
+      `);
+    });
   });
 });
 
