2b6d1df63ac4d7620dc3483e58313ba8a78e3b3e
jaipaljadeja <jadejajaipal5@gmail.com>
2024-12-25 20:03:24 +0530

    plugin-mongo: make persistence queries inline

--- packages/plugin-mongo/src/persistence.ts
+++ packages/plugin-mongo/src/persistence.ts
@@ -42,194 +42,122 @@ export async function persistState<TFilter>(props: {
 }) {
   const { db, session, endCursor, filter, indexerName = "default" } = props;
 
-  const mongoPersistence = new MongoPersistence<TFilter>(
-    db,
-    session,
-    indexerName,
-  );
+  if (endCursor) {
+    await db.collection<CheckpointSchema>(checkpointCollectionName).updateOne(
+      { id: indexerName },
+      {
+        $set: {
+          orderKey: Number(endCursor.orderKey),
+          uniqueKey: endCursor.uniqueKey,
+        },
+      },
+      { upsert: true, session },
+    );
 
-  await mongoPersistence.put({ cursor: endCursor, filter });
+    if (filter) {
+      // Update existing filter's to_block
+      await db
+        .collection<FilterSchema>(filterCollectionName)
+        .updateMany(
+          { id: indexerName, toBlock: null },
+          { $set: { toBlock: Number(endCursor.orderKey) } },
+          { session },
+        );
+
+      // Insert new filter
+      await db.collection<FilterSchema>(filterCollectionName).updateOne(
+        {
+          id: indexerName,
+          fromBlock: Number(endCursor.orderKey),
+        },
+        {
+          $set: {
+            filter: filter as Record<string, unknown>,
+            fromBlock: Number(endCursor.orderKey),
+            toBlock: null,
+          },
+        },
+        { upsert: true, session },
+      );
+    }
+  }
 }
 
 export async function getState<TFilter>(props: {
   db: Db;
   session: ClientSession;
   indexerName?: string;
-}) {
+}): Promise<{ cursor?: Cursor; filter?: TFilter }> {
   const { db, session, indexerName = "default" } = props;
-  const mongoPersistence = new MongoPersistence<TFilter>(
-    db,
-    session,
-    indexerName,
-  );
-  return await mongoPersistence.get();
-}
 
-export async function invalidateState<TFilter>(props: {
-  db: Db;
-  session: ClientSession;
-  cursor: Cursor;
-  indexerName?: string;
-}) {
-  const { db, session, cursor, indexerName = "default" } = props;
+  let cursor: Cursor | undefined;
+  let filter: TFilter | undefined;
 
-  const mongoPersistence = new MongoPersistence<TFilter>(
-    db,
-    session,
-    indexerName,
-  );
+  const checkpointRow = await db
+    .collection<CheckpointSchema>(checkpointCollectionName)
+    .findOne({ id: indexerName }, { session });
 
-  await mongoPersistence.invalidate(cursor);
-}
-
-export async function finalizeState<TFilter>(props: {
-  db: Db;
-  session: ClientSession;
-  cursor: Cursor;
-  indexerName?: string;
-}) {
-  const { db, session, cursor, indexerName = "default" } = props;
-
-  const mongoPersistence = new MongoPersistence<TFilter>(
-    db,
-    session,
-    indexerName,
-  );
-
-  await mongoPersistence.finalize(cursor);
-}
-
-export class MongoPersistence<TFilter> {
-  constructor(
-    private db: Db,
-    private session: ClientSession,
-    private indexerName = "default",
-  ) {}
-
-  public async get(): Promise<{ cursor?: Cursor; filter?: TFilter }> {
-    const cursor = await this._getCheckpoint();
-    const filter = await this._getFilter();
-
-    return { cursor, filter };
-  }
-
-  public async put({ cursor, filter }: { cursor?: Cursor; filter?: TFilter }) {
-    if (cursor) {
-      await this._putCheckpoint(cursor);
-
-      if (filter) {
-        await this._putFilter(filter, cursor);
-      }
-    }
-  }
-
-  public async finalize(cursor: Cursor) {
-    await this._finalizeFilter(cursor);
-  }
-
-  public async invalidate(cursor: Cursor) {
-    await this._invalidateFilter(cursor);
-  }
-
-  // --- CHECKPOINTS METHODS ---
-
-  private async _getCheckpoint(): Promise<Cursor | undefined> {
-    const checkpoint = await this.db
-      .collection<CheckpointSchema>(checkpointCollectionName)
-      .findOne({ id: this.indexerName }, { session: this.session });
-
-    if (!checkpoint) return undefined;
-
-    return {
-      orderKey: BigInt(checkpoint.orderKey),
-      uniqueKey: checkpoint.uniqueKey,
+  if (checkpointRow) {
+    cursor = {
+      orderKey: BigInt(checkpointRow.orderKey),
+      uniqueKey: checkpointRow.uniqueKey,
     };
   }
 
-  private async _putCheckpoint(cursor: Cursor) {
-    await this.db
-      .collection<CheckpointSchema>(checkpointCollectionName)
-      .updateOne(
-        { id: this.indexerName },
-        {
-          $set: {
-            orderKey: Number(cursor.orderKey),
-            uniqueKey: cursor.uniqueKey,
-          },
-        },
-        { upsert: true, session: this.session },
-      );
-  }
-
-  // --- FILTERS METHODS ---
-
-  private async _getFilter(): Promise<TFilter | undefined> {
-    const filter = await this.db
-      .collection<FilterSchema>(filterCollectionName)
-      .findOne(
-        {
-          id: this.indexerName,
-          toBlock: null,
-        },
-        { session: this.session },
-      );
-
-    if (!filter) return undefined;
-
-    return filter.filter as TFilter;
-  }
-
-  private async _putFilter(filter: TFilter, endCursor: Cursor) {
-    // Update existing filter's to_block
-    await this.db
-      .collection<FilterSchema>(filterCollectionName)
-      .updateMany(
-        { id: this.indexerName, toBlock: null },
-        { $set: { toBlock: Number(endCursor.orderKey) } },
-        { session: this.session },
-      );
-
-    // Insert new filter
-    await this.db.collection<FilterSchema>(filterCollectionName).updateOne(
+  const filterRow = await db
+    .collection<FilterSchema>(filterCollectionName)
+    .findOne(
       {
-        id: this.indexerName,
-        fromBlock: Number(endCursor.orderKey),
+        id: indexerName,
+        toBlock: null,
       },
-      {
-        $set: {
-          filter: filter as Record<string, unknown>,
-          fromBlock: Number(endCursor.orderKey),
-          toBlock: null,
-        },
-      },
-      { upsert: true, session: this.session },
+      { session },
     );
+
+  if (filterRow) {
+    filter = filterRow.filter as TFilter;
   }
 
-  private async _invalidateFilter(cursor: Cursor) {
-    await this.db
-      .collection<FilterSchema>(filterCollectionName)
-      .deleteMany(
-        { id: this.indexerName, fromBlock: { $gt: Number(cursor.orderKey) } },
-        { session: this.session },
-      );
+  return { cursor, filter };
+}
 
-    await this.db
-      .collection<FilterSchema>(filterCollectionName)
-      .updateMany(
-        { id: this.indexerName, toBlock: { $gt: Number(cursor.orderKey) } },
-        { $set: { toBlock: null } },
-        { session: this.session },
-      );
-  }
+export async function invalidateState(props: {
+  db: Db;
+  session: ClientSession;
+  cursor: Cursor;
+  indexerName?: string;
+}) {
+  const { db, session, cursor, indexerName = "default" } = props;
 
-  private async _finalizeFilter(cursor: Cursor) {
-    await this.db.collection<FilterSchema>(filterCollectionName).deleteMany(
-      {
-        id: this.indexerName,
-        toBlock: { $lt: Number(cursor.orderKey) },
-      },
-      { session: this.session },
+  await db
+    .collection<FilterSchema>(filterCollectionName)
+    .deleteMany(
+      { id: indexerName, fromBlock: { $gt: Number(cursor.orderKey) } },
+      { session },
     );
-  }
+
+  await db
+    .collection<FilterSchema>(filterCollectionName)
+    .updateMany(
+      { id: indexerName, toBlock: { $gt: Number(cursor.orderKey) } },
+      { $set: { toBlock: null } },
+      { session },
+    );
+}
+
+export async function finalizeState(props: {
+  db: Db;
+  session: ClientSession;
+  cursor: Cursor;
+  indexerName?: string;
+}) {
+  const { db, session, cursor, indexerName = "default" } = props;
+
+  await db.collection<FilterSchema>(filterCollectionName).deleteMany(
+    {
+      id: indexerName,
+      toBlock: { $lt: Number(cursor.orderKey) },
+    },
+    { session },
+  );
 }
