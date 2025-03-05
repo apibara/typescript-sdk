3994544994e2027e2a5f6f2f4b238cbfb16d3509
jaipaljadeja <jadejajaipal5@gmail.com>
2025-03-06 03:20:14 +0530

    indexer: add getDrizzleDB method to vcr

--- packages/indexer/src/testing/index.ts
+++ packages/indexer/src/testing/index.ts
@@ -1,7 +1,12 @@
 import { createClient } from "@apibara/protocol";
 import ci from "ci-info";
+import type { PgDatabase, PgQueryResultHKT } from "drizzle-orm/pg-core";
 import { type IndexerWithStreamConfig, createIndexer } from "../indexer";
-import { type InternalContext, internalContext } from "../plugins/context";
+import {
+  type InternalContext,
+  internalContext,
+  useInternalContext,
+} from "../plugins/context";
 import { logger } from "../plugins/logger";
 import type { CassetteOptions, VcrConfig } from "../vcr/config";
 import { isCassetteAvailable } from "../vcr/helper";
@@ -9,6 +14,8 @@ import { record } from "../vcr/record";
 import { replay } from "../vcr/replay";
 
 export function createVcr() {
+  let drizzleDB: PgDatabase<PgQueryResultHKT>;
+
   return {
     async run<TFilter, TBlock>(
       cassetteName: string,
@@ -40,6 +47,13 @@ export function createVcr() {
 
       const indexer = createIndexer(indexerConfig);
 
+      indexer.hooks.hook("run:after", () => {
+        const context = useInternalContext();
+
+        // @ts-ignore drizzleStorageDB missing error.
+        drizzleDB = context["drizzleStorageDB"];
+      });
+
       if (!isCassetteAvailable(vcrConfig, cassetteName)) {
         if (ci.isCI) {
           throw new Error("Cannot record cassette in CI");
@@ -53,6 +67,15 @@ export function createVcr() {
       } else {
         await replay(vcrConfig, indexer, cassetteName);
       }
+    },
+    getDrizzleDB() {
+      if (!drizzleDB) {
+        throw new Error(
+          "Drizzle database not found, call this method only after you have run the indexer",
+        );
+      }
+
+      return drizzleDB;
     },
   };
 }
