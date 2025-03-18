b4c2cde7927fba623e956fb0dae95df4c5753eeb
jaipaljadeja <jadejajaipal5@gmail.com>
2025-03-18 21:49:12 +0530

    indexer: solve load issues with tracer and metrics by creating them in runtime

--- packages/indexer/src/otel.ts
+++ packages/indexer/src/otel.ts
@@ -1,3 +1,30 @@
-import { trace } from "@opentelemetry/api";
+import { metrics, trace } from "@opentelemetry/api";
 
-export const tracer = trace.getTracer("@apibara/indexer");
+export function createTracer() {
+  return trace.getTracer("@apibara/indexer");
+}
+
+export function createIndexerMetrics() {
+  const meter = metrics.getMeter("@apibara/indexer");
+
+  const currentBlockGauge = meter.createGauge("current_block", {
+    description: "Current block number being processed",
+    unit: "block",
+  });
+
+  const processedBlockCounter = meter.createCounter("processed_block_count", {
+    description: "Number of blocks processed",
+    unit: "blocks",
+  });
+
+  const reorgCounter = meter.createCounter("reorg_count", {
+    description: "Number of reorgs (invalidate messages) received",
+    unit: "reorgs",
+  });
+
+  return {
+    currentBlockGauge,
+    processedBlockCounter,
+    reorgCounter,
+  };
+}
