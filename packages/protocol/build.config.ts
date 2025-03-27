2ef37dddf4bcacef381cad7ff15f7e4850fed462
jaipaljadeja <jadejajaipal5@gmail.com>
2025-03-28 02:50:10 +0530

    protocol: add inhouse parser `codec` to replace `@effect/schema`

--- packages/protocol/build.config.ts
+++ packages/protocol/build.config.ts
@@ -1,7 +1,7 @@
 import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
-  entries: ["./src/index.ts", "./src/testing/index.ts"],
+  entries: ["./src/index.ts", "./src/testing/index.ts", "./src/codec.ts"],
   clean: true,
   outDir: "./dist",
   declaration: true,
