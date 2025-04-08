21b807f40246e95ff06905dd39bae2c3c1dc018e
jaipaljadeja <jadejajaipal5@gmail.com>
2025-04-08 21:03:08 +0530

    example: add presets to apibara config

--- examples/cli-drizzle/apibara.config.ts
+++ examples/cli-drizzle/apibara.config.ts
@@ -9,4 +9,26 @@ export default defineConfig({
       startingBlock: 12_45_000,
     },
   },
+  presets: {
+    mainnet: {
+      runtimeConfig: {
+        evm: {
+          startingBlock: 215_30_000,
+        },
+        starknet: {
+          startingBlock: 12_45_000,
+        },
+      },
+    },
+    testnet: {
+      runtimeConfig: {
+        evm: {
+          startingBlock: 80_60_000,
+        },
+        starknet: {
+          startingBlock: 6_60_000,
+        },
+      },
+    },
+  },
 });
